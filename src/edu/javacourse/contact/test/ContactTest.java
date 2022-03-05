5cc3797d2d23f60962299eacdd87279bd8a6a27f
komron2000 <rayimboevk@gmail.com>
2022-03-05 15:36:56 +0300

    Initial commit

---
+++ src/edu/javacourse/contact/test/ContactTest.java
@@ -0,0 +1,15 @@
+package edu.javacourse.contact.test;
+
+import edu.javacourse.contact.gui.ContactFrame;
+
+/**
+ * Класс для запуска тестовых вызовов
+ * //////FDKFDFDKLDFKLFKLD
+ */
+public class ContactTest 
+{
+    public static void main(String[] args) {
+        ContactFrame cf = new ContactFrame();
+        cf.setVisible(true);
+    }
+}
